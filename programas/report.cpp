5c7beb57f1c0063cd14c9cdfb41ccc33f536e149
jairoegce <jairoegc@gmail.com>
2018-02-20 09:51:34 -0300

    Implementacion final de rutina 2 (grafico valor medio versus posicion)

--- programas/report.cpp
+++ programas/report.cpp
@@ -5,6 +5,7 @@
 #include "TMath.h"
 #include "TGraphErrors.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 #include "TH1F.h"
 #include "TF1.h"
 #include "TSpectrum.h"
@@ -14,23 +15,21 @@ using namespace std;
 //TCanvas *canvas = new TCanvas();
 void rutina0(string fibra);
 void rutina(string fibra);
+void rutina2();
 
 void report(void)
 {
   const char *FIB[] = {"S1up","S2up","S3up","M1up","M2up","F1up","F2up","E1up","E2up"};
   //const char *POS[] = {"-05.1mm","-33.9mm","-62.7mm","-91.5mm"};
+
   for (int i = 0; i<9 ; i++)
   {
     string fib = FIB[i];
-    rutina0(fib);
-    rutina(fib);
-
-    // for (int j = 0; j<4 ; j++)
-    // {
-    //   string pos = POS[j];
-    //   rutina(fib);
-    // }
+    rutina0(fib); // se procesan y juntan en un arbol los datos de cada posicion de una fibra
+    rutina(fib); // se generan un histograma para cada fibra.
   }
+
+  rutina2(); // grafico de valor medio versus posicion de cada fibra
 }
 
 void rutina0(string fibra)
@@ -71,7 +70,6 @@ void rutina0(string fibra)
 
 void rutina(string fibra)
 {
-  string figuresDir = "../../../figures/";
   string target = "./auxFile.root";
   TFile* file = new TFile(target.c_str(),"read");
 	TTree* info = (TTree*)file->Get("auxTree");
@@ -134,6 +132,7 @@ void rutina(string fibra)
   //fmax->SetParameters(maxG0,maxG1,maxG2);
 
   //exportar figura y guardarla
+  string figuresDir = "../../../figures/";
   string figureName = figuresDir + fibra + ".pdf";
   cout << "saving figure as :" << figureName.c_str() << endl;
   canvas->SaveAs(figureName.c_str());
@@ -141,26 +140,37 @@ void rutina(string fibra)
 
 void rutina2()
 {
-  //Configuracion de directorio y nombre de archivos
+  // Configuracion de directorio y nombre de archivos
   const char *FIB[] = {"S1up","S2up","S3up","M1up","M2up","F1up","F2up","E1up","E2up"};
   const char *POS[] = {"-05.1mm","-33.9mm","-62.7mm","-91.5mm"};
   float MEAN[4], DESV[4], POSX[4]={5.1,33.9,62.7,91.5}, ePOSX[4]={0,0,0,0};
 
+  // Creacion de canvas y objetos relativos al grafico final
   TCanvas *grafico = new TCanvas();
   TGraph *tge[9];
+  auto legend = new TLegend(0.9,0.7,0.52,0.9);
+  legend->SetNColumns(2);
 
+  // Lectura y procesamiento de los datos
   string arbolesDir = "../../../arboles/";
   TFile* file[4];
   Float_t aMPPC;
   Float_t vPIN;
 
+  //Variables para el procesamiento de datos
   float sum = 0;
   float sum2 = 0;
+  float sumvPIN = 0;
+  float meanvPIN = 0;
   int nEvents = 0;
 
+  // Procesamiento de cada fibra
+  int ngraph = 0;
   for (int i=0; i<9; i++)
   {
     string fib = FIB[i];
+
+    //Procesamiento de cada posicion
     for (int j=0; j<4; j++)
     {
       string pos = POS[j];
@@ -174,16 +184,21 @@ void rutina2()
       info->SetBranchAddress("aMPPC",&aMPPC);
       info->SetBranchAddress("vPIN",&vPIN);
 
-      // Calculo de media y desviacion estandar
+      //Media y desviacion estandar (normalizados por valor de vPIN) para cada posicion
       nEvents = info->GetEntries();
       for ( int k = 0; k<nEvents; k++)
       {
         info->GetEntry(k);
-        sum += aMPPC;
-        sum2 += aMPPC*aMPPC;
+        sum += aMPPC/vPIN;
+        sum2 += (aMPPC/vPIN)*(aMPPC/vPIN);
+        sumvPIN += vPIN;
       }
-      MEAN[j] = sum/nEvents;
-      DESV[j] = TMath::Sqrt((sum2/nEvents)-(MEAN[j]*MEAN[j]));
+
+      meanvPIN = sumvPIN/nEvents;
+
+      // Resultados
+      MEAN[j] = (sum/nEvents)*meanvPIN;
+      DESV[j] = TMath::Sqrt((sum2/nEvents)*meanvPIN*meanvPIN-(MEAN[j]*MEAN[j]));
 
       cout << "-----------------------------------------------"<<endl;
       cout << (fib+pos).c_str() <<endl;
@@ -196,13 +211,44 @@ void rutina2()
       file[j]->Close();
       sum = 0;
       sum2 = 0;
+      sumvPIN = 0;
 
     }
+
+    // Grafico por fibra
     tge[i] = new TGraph(4, POSX, MEAN);
-    grafico->cd();
-    tge[i]->Draw("same");
+
+    // Se configura el grafico en el primer elemento a graficar
+    if (ngraph == 0)
+    {
+      tge[i]->SetTitle("Amplitud media v/s Posicion");
+      tge[i]->GetXaxis()->SetTitle("Posicion [mm]");
+      tge[i]->GetYaxis()->SetTitle("Amplitud Media [v]");
+      tge[i]->SetLineColor((i+1));
+      tge[i]->GetHistogram()->SetMaximum(12.);
+      tge[i]->GetHistogram()->SetMinimum(0.);
+      tge[i]->Draw("AL*");
+      ngraph = 1;
+    }
+    else
+    {
+      tge[i]->SetLineColor((i+1));
+      tge[i]->Draw("L*");
+    }
+
+    // Se agrega cada curva a la simbologia del grafico
+    legend->AddEntry(tge[i],fib.c_str(),"lp");
+
 
   }
+
+  // Se dibuja la leyenda
+  legend->Draw();
+
+  // Se guarda el canvas final en formato pdf
+  grafico->SaveAs("../../../figures/mediaVSposicion.pdf");
+
+
 
   return;
 }
